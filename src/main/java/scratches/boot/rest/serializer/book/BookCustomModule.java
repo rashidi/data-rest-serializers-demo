ec40762ad7c712ba56937265980d647d11de7eb9
Rashidi Zin <rashidi@zin.my>
2019-11-15 14:59:37 +0800

    Add missing serializer

--- src/main/java/scratches/boot/rest/serializer/book/BookCustomModule.java
+++ src/main/java/scratches/boot/rest/serializer/book/BookCustomModule.java
@@ -2,6 +2,7 @@ package scratches.boot.rest.serializer.book;
 
 import com.fasterxml.jackson.databind.module.SimpleDeserializers;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.module.SimpleSerializers;
 import lombok.AllArgsConstructor;
 import scratches.boot.rest.serializer.author.AuthorRepository;
 
@@ -15,8 +16,10 @@ public class BookCustomModule extends SimpleModule {
 
     @Override
     public void setupModule(SetupContext context) {
+        SimpleSerializers serializers = new SimpleSerializers();
         SimpleDeserializers deserializers = new SimpleDeserializers();
 
+        serializers.addSerializer(Book.class, new BookJsonSerializer(authorRepository));
         deserializers.addDeserializer(Book.class, new BookJsonDeserializer(authorRepository));
 
         context.addDeserializers(deserializers);
